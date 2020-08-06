41b3e5a50ff8e4a83b9a7fc1b19a2bab6bd52864
Jörg Jooss (MSFT) <joerg.jooss@microsoft.com>
2020-08-06 15:35:15 +0200

    feat: PostgreSQL support for Oauth2 script

--- grafana/setup-oauth2.sh
+++ grafana/setup-oauth2.sh
@@ -4,9 +4,11 @@ set -e
 #Set defaults
 default_tag=latest
 default_location=westeurope
-default_rg=azure-toolbox
+default_rg=grafana-on-azure
 default_plan=grafana-linux-plan
-default_sku=B1
+default_appsvc_sku=B1
+default_postgresql_sku=B_Gen5_1
+default_postgresql_storage=51200
 
 #Set script variables
 webapp=$1
@@ -14,16 +16,24 @@ tag=$2
 location=$3
 rg=$4
 plan=$5
-sku=$6
+appsvc_sku=$6
+postgresql_sku=$7
+postgresql_storage=$8
 
 function show_usage_and_exit {
-  echo "Usage: setup-grafana.azcli <webapp> [<tag>] [<location>] [<resource-group>] [<app-service-plan>] [<sku>]"
+  echo "This script creates an Azure App Service running Grafana with an Azure Database for PostgreSQL as Grafana database."
+  echo "Grafana will be integrated with your current Azure AD tenant and your current Azure CLI user will be made a"
+  echo "Grafana admininistrator."
+  echo
+  echo "Usage: setup-oauth2.sh <webapp> [<tag>] [<location>] [<resource-group>] [<app-service-plan>] [<app-service-sku>] [<postgresql-sku>] [<postgresql-storage>]"
   echo "<webapp> specifies the Web App name, i.e. <webapp>.azurewebsites.net. Mandatory."
   echo "<tag> specifies the Grafana Docker image tag. Defaults to \"$default_tag\"."
   echo "<location> specifies the Azure region. Defaults to \"$default_location\"."
   echo "<resource-group> specifies the resource group. Defaults to \"$default_rg\"."
   echo "<app-service-plan> specifies the App Service plan. Defaults to \"$default_plan\"."
-  echo "<sku> specifies the App Service SKU. Defaults to \"$default_sku\"."
+  echo "<app-service-sku> specifies the App Service SKU. Defaults to \"$default_appsvc_sku\"."
+  echo "<postgresql-sku> specifies the Azure Database for PostgreSQL SKU. Defaults to \"$default_postgresql_sku\"."
+  echo "<postgresql-storage> specifies the Azure Database for PostgreSQL storage in MB. Defaults to \"$default_postgresql_storage\" MB."
   exit 1
 }
 
@@ -47,14 +57,22 @@ if [ -z $plan ]; then
   plan=$default_plan
 fi
 
-if [ -z $sku ]; then
-  sku=$default_sku
+if [ -z $appsvc_sku ]; then
+  appsvc_sku=$default_appsvc_sku
+fi
+
+if [ -z $postgresql_sku ]; then
+  postgresql_sku=$default_postgresql_sku
+fi
+
+if [ -z $postgresql_storage ]; then
+  postgresql_storage=$default_postgresql_storage
 fi
 
 # Register Azure AD application and service principal
 aad_tenant_id=$(az account show --query tenantId -o tsv)
-echo "Registering application for Grafana in Azure AD in tenant \"$aad_tenant_id\"..."
-time_stamp=$(date --utc +"%s")
+echo -n "Registering application for Grafana in Azure AD in tenant \"$aad_tenant_id\"..."
+time_stamp=$(date --utc +%Y%m%d-%H%M%S)
 aad_app_secret=$(uuidgen)
 aad_app_id=$(az ad app create \
   --display-name "Grafana Azure App Service" \
@@ -66,42 +84,99 @@ aad_app_id=$(az ad app create \
   --query appId \
   -o tsv)
 az ad sp create --id $aad_app_id -o none
+echo "done."
+echo "Created application with client ID \"$aad_app_id\"."
+
 
 # Create configuration settings
-echo "Generating Docker Compose file..."
 admin_pwd=$(uuidgen)
+db_host=$webapp-db
+db_user=grafana
+db_pwd=$(uuidgen)
+
+echo -n "Generating Docker Compose file..."
 cp docker-compose.oauth2.yml docker-compose.generated.yml
 sed -i "s/__1__/$tag/g" docker-compose.generated.yml
 sed -i "s/__2__/$aad_app_id/g" docker-compose.generated.yml
 sed -i "s/__3__/$aad_app_secret/g" docker-compose.generated.yml
 sed -i "s/__4__/$aad_tenant_id/g" docker-compose.generated.yml
 sed -i "s/__5__/$admin_pwd/g" docker-compose.generated.yml
+sed -i "s/__6__/$db_host/g" docker-compose.generated.yml
+sed -i "s/__7__/$db_user@$db_host/g" docker-compose.generated.yml
+sed -i "s/__8__/$db_pwd/g" docker-compose.generated.yml
+echo "done."
 
-echo "Generating Web App configuration..."
+echo -n "Generating Web App configuration..."
 cp appSettings.oauth2.json appSettings.generated.json
 sed -i "s/__1__/$webapp/g" appSettings.generated.json
 sed -i "s/__2__/$aad_app_id/g" appSettings.generated.json
 sed -i "s/__3__/$aad_app_secret/g" appSettings.generated.json
 sed -i "s/__4__/$aad_tenant_id/g" appSettings.generated.json
 sed -i "s/__5__/$admin_pwd/g" appSettings.generated.json
+sed -i "s/__6__/$db_host/g" appSettings.generated.json
+sed -i "s/__7__/$db_user@$db_host/g" appSettings.generated.json
+sed -i "s/__8__/$db_pwd/g" appSettings.generated.json
+echo "done."
 
 # Create Azure resources
-echo "Creating resource group \"$rg\" in \"$location\"..."
+echo -n "Creating resource group \"$rg\" in \"$location\"..."
 az group create \
   -g $rg \
   -l $location \
   -o none
+echo "done."
 
-echo "Creating App Service plan \"$plan\" of size \"$sku\"..."
+echo -n "Creating Azure Database for PostgreSQL..."
+az postgres server create \
+  -g $rg \
+  -n $db_host \
+  -l $location \
+  --sku-name $postgresql_sku \
+  --version 11 \
+  --storage-size $postgresql_storage \
+  --admin-user $db_user \
+  --admin-password $db_pwd \
+  --minimal-tls-version TLS1_2 \
+  -o none
+echo "done."
+
+echo -n "Creating Grafana database..."
+az postgres db create \
+  -g $rg \
+  -n grafana \
+  -s $db_host \
+  -o none
+echo "done."
+
+echo -n "Updating Azure Database firewall rules..."
+current_ip=$(curl -s 'https://api.ipify.org?format=text')
+az postgres server firewall-rule create \
+  -g $rg \
+  -n "from-host-$time_stamp" \
+  -s $db_host \
+  --start-ip-address $current_ip \
+  --end-ip-address $current_ip \
+  -o none
+az postgres server firewall-rule create \
+  -g $rg \
+  -n all-azure \
+  -s $db_host \
+  --start-ip-address 0.0.0.0 \
+  --end-ip-address 0.0.0.0 \
+  -o none
+echo "done."
+
+echo -n "Creating App Service plan \"$plan\" of size \"$appsvc_sku\"..."
 az appservice plan create \
   -g $rg \
   -n $plan \
   -l $location \
   --is-linux \
-  --sku $sku \
+  --sku $appsvc_sku \
   -o none
+echo "done."
 
-echo "Creating Web App \"$webapp\" using \"grafana/grafana:$tag\"..."
+echo -n "Creating Web App \"$webapp\" using \"grafana/grafana:$tag\"..."
 az webapp create \
   -g $rg \
   -n $webapp \
@@ -113,8 +188,9 @@ az webapp update \
   -n $webapp \
  --https-only true \
  -o none
+echo "done."
 
-echo "Updating Web App configuration..."
+echo -n "Updating Web App configuration..."
 az webapp config appsettings set \
   -g $rg \
   -n $webapp \
@@ -125,8 +201,9 @@ az webapp config set \
   -n $webapp \
   --always-on true \
   -o none
+echo "done."
 
-# Provision AAD user
+# Add AAD user to Grafana
 user_name=$(az ad signed-in-user show --query mail -o tsv)
 if [ -z $user_name ]; then
   user_name=$(az ad signed-in-user show --query otherMails[0] -o tsv)
@@ -148,4 +225,4 @@ curl -s -X PATCH -u "admin:$admin_pwd" \
   "https://$webapp.azurewebsites.net/api/org/users/$id" \
   | jq -r ".message"
 
-echo "Done. Open https://$webapp.azurewebsites.net or run \"docker-compose -f docker-compose.generated.yml\"."
+echo "Setup complete. Open https://$webapp.azurewebsites.net or run \"docker-compose -f docker-compose.generated.yml\"."
